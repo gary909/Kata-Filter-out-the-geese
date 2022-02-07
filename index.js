94e8df691f0825327c038f9f9d2556eedf75097a
gary <gary.white@lodgit.com>
2022-02-07 14:39:34 +0100

    code working

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 function gooseFilter (birds) {
-    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
-    var myArr = [];
-    for (var i = 0; i < birds.length; i++){
-        // myArr = myArr + birds[i] + ' ,';
-        myArr.push(birds[i]);
-    }
-        return myArr;
-  };
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+    return birds.filter((elm) => !geese.includes(elm))
+};
 
-  console.log(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"])); // Return ["Mallard", "Hook Bill", "Crested", "Blue Swedish"
-  console.log(gooseFilter(["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"])); // Return [["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"]
+console.log(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"])); // Return ["Mallard", "Hook Bill", "Crested", "Blue Swedish"
+console.log(gooseFilter(["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"])); // Return [["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"]
