01173bea62c5e8aa90b274d6e9cc1284370b52d1
gary <gary.white@lodgit.com>
2022-02-07 13:14:06 +0100

    code

---
+++ index.js
@@ -0,0 +1,8 @@
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    
+    // return an array containing all of the strings in the input array except those that match strings in geese
+  };
+
+  console.log(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"])); // Return ["Mallard", "Hook Bill", "Crested", "Blue Swedish"
+  console.log(gooseFilter(["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"])); // Return [["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"]
